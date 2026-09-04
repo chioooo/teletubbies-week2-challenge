c676fcd0967a04c01f26dcf4a7e7709fa72dd619
rzt20 <226w0702@zongolica.tecnm.mx>
2026-09-04 17:15:55 -0600

    Practica-w02-rene

--- shop-cart-app/src/main/java/org/example/Main.java
+++ shop-cart-app/src/main/java/org/example/Main.java
@@ -4,5 +4,21 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // TODO: prueba tu Cart aquí una vez que Product/Cart estén implementados.
+        Product physicalProduct = new PhysicalProduct("SKU123", "Physical Product", 100.0, 10);
+        Product digitalProduct = new DigitalProduct("SKU456", "Digital Product", 100.0, 5);
+
+        Cart cart = new Cart();
+        System.out.println("Total: " + cart.total());
+        System.out.println("Items in cart: " + cart.getItems());
+
+        try {
+            cart.addProduct(physicalProduct, 2);
+            cart.addProduct(digitalProduct, 1);
+        } catch (InvalidQuantityException | InsufficientStockException e) {
+            System.err.println("Error adding product to cart: " + e.getMessage());
+        }
+        System.out.println("Total after adding products: " + cart.total());
+        System.out.println("Items in cart after adding products: " + cart.getItems());
     }
 }
+
